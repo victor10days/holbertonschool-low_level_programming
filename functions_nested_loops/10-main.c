78aac70a8ba65cabfc7c6c586c090e72630fd945
victor10days <victor10days@gmail.com>
2025-02-27 09:27:20 -0400

    a + b

---
+++ functions_nested_loops/10-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point to check the add function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+
+	return (0);
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
+int add(int a, int b);
 
 #endif /* MAIN_H */
-
 
