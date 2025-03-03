700c82b7fec107332afab7ba1c3c597433fddcbd
victor10days <victor10days@gmail.com>
2025-03-03 18:54:28 -0400

    I believe in numbers and signs

--- more_functions_nested_loops/4-main.c
+++ more_functions_nested_loops/4-main.c
@@ -1,13 +1,14 @@
+/* 4-main.c - Main file for testing print_most_numbers function */
 #include "main.h"
 
 /**
- * main - Entry point to test print_most_numbers function
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	print_most_numbers();
-	return (0);
+    print_most_numbers();
+    return (0);
 }
 
