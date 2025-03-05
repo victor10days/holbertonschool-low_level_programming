b25fafc0ea3f18c260b54619a4c701c074a403fd
victor10days <victor10days@gmail.com>
2025-03-05 16:24:42 -0400

    I am kind of paranoid in reverse. I suspect people of plotting to make me happy

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,16 +1,37 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
+ * print_array - Prints an array of integers.
+ * @a: An array of integers.
+ * @n: The number of elements to print.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+			printf(", ");
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * main - Tests the reverse_array function.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char *str;
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
+	print_array(a, sizeof(a) / sizeof(int));
+	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+
 	return (0);
 }
 
