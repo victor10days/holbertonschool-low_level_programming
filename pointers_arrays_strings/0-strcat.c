0b16f5e96ce0365eb079fa9c39683f9fcc1811bf
victor10days <victor10days@gmail.com>
2025-03-05 16:01:26 -0400

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,25 +9,25 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    /* Find the end of dest */
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
+	/* Find the end of dest */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-    /* Append src to dest */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	/* Append src to dest */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    /* Add the null terminator */
-    dest[i] = '\0';
+	/* Add the null terminator */
+	dest[i] = '\0';
 
-    return (dest);
+	return (dest);
 }
 
