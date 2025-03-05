78aa64a86da2baad3dd031ed4b985146cb29607c
victor10days <victor10days@gmail.com>
2025-03-05 15:19:06 -0400

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@
 int _atoi(char *s)
 {
 	int i = 0, sign = 1;
-	unsigned int num = 0; /* Use unsigned int to avoid overflow */
+	unsigned int num = 0;
 
 	while (s[i] != '\0')
 	{
@@ -18,13 +18,14 @@ int _atoi(char *s)
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (num > 214748364 || (num == 214748364 && s[i] > '7')) /* Check for INT_MAX overflow */
+			if (num > 214748364 ||
+			    (num == 214748364 && s[i] > '7'))
 			{
 				return (sign == 1 ? 2147483647 : -2147483648);
 			}
 			num = num * 10 + (s[i] - '0');
 		}
-		else if (num > 0) /* Stop conversion once a number is found and non-digit appears */
+		else if (num > 0)
 			break;
 
 		i++;
