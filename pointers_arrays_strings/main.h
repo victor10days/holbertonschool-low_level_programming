ba9cfe3f258098521c61c09ade048c2178fbdf87
victor10days <victor10days@gmail.com>
2025-03-05 14:54:35 -0400

    Half the lies they tell about me aren't true

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,13 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void swap_int(int *a, int *b);
-void reset_to_98(int *n);
-int _strlen(char *s);
-void _puts(char *str);
 int _putchar(char c);
-void print_rev(char *s);
-void rev_string(char *s);
+void puts2(char *str);
 
 #endif /* MAIN_H */
 
