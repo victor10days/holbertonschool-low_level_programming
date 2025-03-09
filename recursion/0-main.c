2d9c6f5c450385c2e22097250f4ad8b72be85306
victor10days <victor10days@gmail.com>
2025-03-09 19:09:09 -0400

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ recursion/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test _puts_recursion function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
+
