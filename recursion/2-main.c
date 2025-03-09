dda2ded57498d23d3405aa26418c264ef6debb0a
victor10days <victor10days@gmail.com>
2025-03-09 19:29:11 -0400

    feel real

---
+++ recursion/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - tests _strlen_recursion function.
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
+
