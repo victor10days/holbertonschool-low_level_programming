79ccadbc3b718fd403cc8a8f228bb6e83f19a020
victor10days <victor10days@gmail.com>
2025-03-09 22:39:38 -0400

    It ain't

---
+++ argc_argv/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* Task 0 */
+int main(int argc, char *argv[]);
+
+/* Task 1 */
+int main(int argc, char *argv[]);
+
+/* Task 2 */
+int main(int argc, char *argv[]);
+
+/* Task 3 */
+int main(int argc, char *argv[]);
+
+/* Task 4 */
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
+
