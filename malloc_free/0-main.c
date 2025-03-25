c54e834310b2e90a142150e7ca512f207f32dcf1
victor10days <victor10days@gmail.com>
2025-03-25 15:43:17 -0400

    0-create_array first committ

---
+++ malloc_free/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main(void)
+{
+    char *buffer;
+
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    simple_print_buffer(buffer, 98);
+    free(buffer);
+    return (0);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+void simple_print_buffer(char *buffer, unsigned int size);
+
+#endif /*MAIN_H*/
